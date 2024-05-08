711813f3bee3fdfe7798c2fa1226ea184e9f414b
Diego Iturra <diturra@dparadig.com>
2024-05-08 19:47:40 -0400

    fix: add join with age_Range in studen get methods

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,18 @@
 class StudentsController < BaseController
+    def index
+        @students = Student.includes(:kinder_garden, :age_range)
+        render json: @students, include: [:kinder_garden, :age_range], status: :ok
+    end
+
+    def show
+        @student = Student.includes(:kinder_garden, :age_range).find(params[:id])
+        render json: @student, include: [:kinder_garden, :age_range], status: :ok
+    end
+
+    private
+    def student_params
+        params.require(:student).permit(:rut, :names, :father_lastname, 
+            :mother_lastname, :birthday, :gender, :group, :comments, :email, 
+            :password, :state, :kinder_garden_id, :age_range_id)
+    end
 end

3a73fef4523cc9ec2c80f7f795d8c98818793514
Diego Iturra <diturra@dparadig.com>
2024-05-26 20:21:18 -0400

    feat: add a controller to display all and find by id assignments

--- app/controllers/teacher_students_controller.rb
+++ app/controllers/teacher_students_controller.rb
@@ -1,2 +1,11 @@
 class TeacherStudentsController < BaseController
+    def index
+        all_assignments = TeacherStudent.includes(:teacher, :student)
+        render json: all_assignments, include: [:teacher, :student], status: :ok
+    end
+
+    def show
+        assignment = TeacherStudent.includes(:teacher, :student).find(params[:id])
+        render json: assignment, include: [:teacher, :student], status: :ok
+    end
 end

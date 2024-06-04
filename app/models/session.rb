f307d56f515e8879ba2c5d48b6b94a21ff9ae567
Diego Iturra <diturra@dparadig.com>
2024-06-03 21:57:52 -0400

    feat: add validation to session model

--- app/models/session.rb
+++ app/models/session.rb
@@ -3,4 +3,14 @@ class Session < ApplicationRecord
     belongs_to :student
     belongs_to :tablet
     has_many :activity , through: :activity_session
+
+    validates :state, presence: true
+
+    validates :state, 
+        presence: true,
+        inclusion: { 
+            in: %w(complete incomplete), 
+            message: "%{value} is not a valid state" 
+        }, 
+        allow_nil: false,
 end

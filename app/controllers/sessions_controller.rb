10c5236ad95e08704d59363f3da5e0a3b6051204
Diego Iturra <diturra@dparadig.com>
2024-06-08 22:02:48 -0400

    :sparkles: feat: add a new route to get all sessions filter by teacher id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,10 @@
 class SessionsController < BaseController
+
+    def find_by_teacher
+        sessions = Session.where(teacher_id: params[:teacher_id])
+                    .includes(:student)
+                    
+        render json: sessions, status: :ok, include: [:student]
+    end
+
 end

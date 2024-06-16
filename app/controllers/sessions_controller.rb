00f00a9c3025199260c07f09dc736a327aa32751
Diego Iturra <diturra@dparadig.com>
2024-06-15 23:21:08 -0400

    fix: overrride method to update to return the data of student included in response body

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,4 +7,22 @@ class SessionsController < BaseController
         render json: sessions, status: :ok, include: [:student]
     end
 
+    def update
+        session = Session.find(params[:id])
+
+        if session.update(resource_params)
+            render json: session, status: :ok, include: [:student]
+        else
+            render json: session.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def resource_params
+        params.require(:session).permit(:date_session, :hour, 
+                :session_number, :state, :duration, :apk_version,
+                :comments, :teacher_id, :student_id, :tablet_id)
+    end
+
 end
